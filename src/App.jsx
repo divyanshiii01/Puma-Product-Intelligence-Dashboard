0b8c71f31a302fa7b561caa3d0863cdd538935a6
divyanshiii01 <dc5626181@gmail.com>
2026-06-09 21:05:16 +0530

    Create hero section for homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
 function App() {
   return (
-    <div>
-      <h1>Puma Analytics Dashboard</h1>
-      <p>Real-Time Product Intelligence</p>
+    <div
+      style={{
+        backgroundColor: "#0f172a",
+        color: "white",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "4rem",
+          marginBottom: "10px",
+        }}
+      >
+        PUMA ANALYTICS
+      </h1>
+
+      <p
+        style={{
+          fontSize: "1.3rem",
+          color: "#94a3b8",
+        }}
+      >
+        Real-Time Product Intelligence
+      </p>
+
+      <button
+        style={{
+          marginTop: "30px",
+          padding: "12px 24px",
+          border: "none",
+          borderRadius: "10px",
+          fontSize: "1rem",
+          cursor: "pointer",
+        }}
+      >
+        Explore Dashboard
+      </button>
     </div>
   );
 }
 
 export default App;
-             
